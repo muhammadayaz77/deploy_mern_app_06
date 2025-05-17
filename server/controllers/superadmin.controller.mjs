9ebc4e0cb22e832440b98182ac14a5dd2d6357da
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2025-05-18 02:23:24 +0500

    Add removeAdmin functionality: implement admin deletion logic in superadmin.controller and update routing for admin removal

--- server/controllers/superadmin.controller.mjs
+++ server/controllers/superadmin.controller.mjs
@@ -22,3 +22,34 @@ export const getAllAdmin = async (req, res) => {
     });
   }
 };
+export const removeAdmin = async (req, res) => {
+  try {
+    // Find users where role is either 'admin1' or 'admin2'
+    const {id} = req.body;
+    if(!id){
+      return res.status(400).json({
+        message: 'Required data to proceed',
+        success: false,
+      });
+    }
+    let removedUser = await User.findByIdAndDelete({_id:id});
+    if(!removedUser){
+      return res.status(400).json({
+        message: 'Admin not found',
+        success: false,
+      });
+    }
+    console.log("not in db : ",removedUser)
+
+    return res.status(200).json({
+      message: 'Admins deleted successfully',
+      success: true,
+    });
+  } catch (error) {
+    console.error('Error fetching admins:', error);
+    return res.status(500).json({
+      message: error.message || 'Failed to fetch admins',
+      success: false
+    });
+  }
+};
--- server/routes/superadmin.route.mjs
+++ server/routes/superadmin.route.mjs
@@ -1,9 +1,10 @@
 import express from 'express'
-import {getAllAdmin} from '../controllers/superadmin.controller.mjs'
+import {getAllAdmin, removeAdmin} from '../controllers/superadmin.controller.mjs'
 import { auth, roleAuth } from '../middleware/auth.middleware.mjs';
 
 const router = express.Router();
 
 router.get('/get-all-admin',auth,roleAuth(['sup_admin']),getAllAdmin)
+router.post('/remove-admin',auth,roleAuth(['sup_admin']),removeAdmin)
 
 export default router
